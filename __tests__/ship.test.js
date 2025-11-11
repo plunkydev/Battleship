cb35f77fe756fcb2cdc0a1153c26dc15a21c3f71
plunkydev <plunkydesign@gmail.com>
2025-11-11 15:11:22 -0500

    refactor: simplify Ship tests using beforeEach for cleaner setup

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -2,27 +2,29 @@
 import { Ship } from '../src/Ship';
 
 describe('Ship Class', () => {
-    test('instance.length should be 2', () => {
-        let destroyer = new Ship(2)
-        expect(destroyer.length).toBe(2)
+    let ship;
+
+    beforeEach(() => {
+        ship = new Ship(3);
+    });
+    test('instance.length should be 3', () => {
+        expect(ship.length).toBe(3)
     });
 
     test('Ship should start with 0 hits', () => {
-        let carrier = new Ship(3) 
-        expect(carrier.hits).toBe(0)
+        expect(ship.hits).toBe(0)
     });
 
     test('hit should increse hits by 1', () => {
-        let cruiser = new Ship(3)
-        cruiser.hit()
-        expect(cruiser.hits).toBe(1)
+        ship.hit()
+        expect(ship.hits).toBe(1)
     });
 
     test('isSunk should be true when length is equal hits', () => {
-        let destroyer = new Ship(2)
-        destroyer.hit()
-        destroyer.hit()
-        expect(destroyer.hits).toBe(destroyer.length)
-        expect(destroyer.isSunk()).toBe(true)
+        ship.hit()
+        ship.hit()
+        ship.hit()
+        expect(ship.hits).toBe(ship.length)
+        expect(ship.isSunk()).toBe(true)
     });
 });
