a51eea6dd22a795fbe9aad898c724813246bdb80
plunkydev <plunkydesign@gmail.com>
2025-12-29 14:27:51 -0500

    refactor(GameController): inject player and computer boards via constructor and return coordinates in playTurn for UI sync

--- src/GameControler.js
+++ src/GameControler.js
@@ -1,31 +1,41 @@
 import { Player } from "./Player.js";
+
 export class GameController {
-    constructor() {
+    constructor(playerBoard, computerBoard) {
         this.player1 = new Player("human");
         this.player2 = new Player("computer");
         this.currentTurn = "human";
         this.winner = null;
+
+        this.player1.board = playerBoard;
+        this.player2.board = computerBoard;
+
     }
+
     playTurn(coordinates) {
-        let hit = false;
+    let hit = false;
+    let usedCoordinates = coordinates;
 
-        if (this.currentTurn === "human") {
-            hit = this.player1.attack(this.player2, coordinates);
-            this.currentTurn = "computer";
-        } else {
-            const random = this.player2.randomAttack(this.player1);
-            hit = this.player2.attack(this.player1, random);
-            this.currentTurn = "human";
-        }
-
-        const gameOver = this.isGameOver();
-
-        return {
-            hit,
-            currentTurn: this.currentTurn,
-            winner: gameOver ? this.winner : null,
-        };
+    if (this.currentTurn === "human") {
+        hit = this.player1.attack(this.player2, coordinates);
+        this.currentTurn = "computer";
+    } else {
+        const randomCoords = this.player2.randomAttack(this.player1);
+        hit = this.player2.attack(this.player1, randomCoords);
+        usedCoordinates = randomCoords; //  coordenadas reales
+        this.currentTurn = "human";
     }
+
+    const gameOver = this.isGameOver();
+
+    return {
+        hit,
+        coordinates: usedCoordinates, // un arreglo [x, y]
+        currentTurn: this.currentTurn,
+        winner: gameOver ? this.winner : null,
+    };
+}
+
 
     isGameOver() {
         if (this.player1.board.allShipsSunk()) {
