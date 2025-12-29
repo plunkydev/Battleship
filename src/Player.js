19d0360f942124644215e9287d4573897ae92fdc
plunkydev <plunkydesign@gmail.com>
2025-12-29 14:32:32 -0500

    refactor(Player): simplify randomAttack to return only coordinates for centralized attack handling in GameController

--- src/Player.js
+++ src/Player.js
@@ -10,15 +10,16 @@ class Player {
         return opponent.board.receiveAttack(coordinates);
     }
     randomAttack(opponent) {
-        let x, y, coordinate;
-        do {
-            x = Math.floor(Math.random() * 10);
-            y = Math.floor(Math.random() * 10);
-            coordinate = [x, y];
-        } while (this.attacks.some(a => a[0] === x && a[1] === y));
-        this.attacks.push(coordinate);
-        return opponent.board.receiveAttack(coordinate);
-    }
+    let x, y, coordinate;
+    do {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * 10);
+        coordinate = [x, y];
+    } while (this.attacks.some(a => a[0] === x && a[1] === y));
+
+    this.attacks.push(coordinate);
+    return coordinate;
+}
 
 }
 
